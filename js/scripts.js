d3c17129b5efd4f399360ba2134d4eee51fb9a39
kayschulze <M@yaB@tCh@va77>
2017-07-18 06:35:22 -0700

    add array elements

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,16 @@ Place.prototype.displayDestination = function() {
   return this.destination;
 };
 
+var myPlaces = [];
+myPlaces[0] = new Place("Burien", "Washington", "Lake Burien", "2007", "College, Career, & Citizenship");
+myPlaces[1] = new Place("Aloha", "Oregon", "Schulze Hotel", "1978", "Not Hawaii");
+
+var i = 0;
 $(document).ready(function() {
+  $("ul#places").append("<li><span class='places'>" + myPlaces[0].destination + "</span></li>");
+  $("ul#places").append("<li><span class='places'>" + myPlaces[1].destination + "</span></li>");
+  console.log("got it!");
+  var newPlace = [];
   $("#new-destination").submit(function(event) {
     event.preventDefault();
 
@@ -20,14 +29,16 @@ $(document).ready(function() {
     var inputtedYear = $("input#new-year").val();
     var inputtedNotes = $("input#new-notes").val();
 
-    var newPlace = new Place(inputtedDestination, inputtedLocation, inputtedLandmark, inputtedYear, inputtedNotes);
+    newPlace[i] = new Place(inputtedDestination, inputtedLocation, inputtedLandmark, inputtedYear, inputtedNotes);
 
-    $("ul#placeName").append("<li>" + newPlace.displayDestination() + "</li>");
+    $("ul#placeName").append("<li>" + newPlace[i].displayDestination() + "</li>");
 
     $("ul#places").append("<li><span class='places'>" + inputtedDestination + "</span></li>" + "<li><span class='places'>" + inputtedLocation + "</span></li>" + "<li><span class='places'>" + inputtedLandmark + "</span></li>" + "<li><span class='places'>" + inputtedYear + "</span></li>" + "<li><span class='places'>" + inputtedNotes + "</span></li>");
 
+    i++;
+
     $("#placeName").click(function() {
-      $("#places").toggle();
+      $("newPlace[i]#places").show();
     });
 
     $("input#new-destination").val("");
@@ -36,4 +47,5 @@ $(document).ready(function() {
     $("input#new-year").val("");
     $("input#new-notes").val("");
   });
+
 });
